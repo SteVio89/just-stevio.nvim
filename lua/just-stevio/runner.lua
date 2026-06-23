aa7a2a5a65ebc9def3482d06962c3203cce9d544
Stefan Viol <stefan@stevio.de>
2026-06-23 22:59:22 +0200

    improve terminal handling (#4)

--- lua/just-stevio/runner.lua
+++ lua/just-stevio/runner.lua
@@ -6,10 +6,25 @@ function M.run(recipe_name, args)
 	local buf = vim.api.nvim_create_buf(false, true)
 	vim.api.nvim_win_set_buf(0, buf)
 
+	vim.keymap.set("n", "q", function()
+		vim.api.nvim_buf_delete(buf, { force = true })
+	end, { buffer = buf, nowait = true })
+
 	local cmd = { "just", recipe_name }
 	vim.list_extend(cmd, args or {})
 
-	vim.fn.jobstart(cmd, { term = true })
+	vim.fn.jobstart(cmd, {
+		term = true,
+		on_exit = function()
+			vim.schedule(function()
+				if vim.api.nvim_get_current_buf() ~= buf then
+					return
+				end
+				local keys = vim.api.nvim_replace_termcodes("<C-\\><C-n>", true, false, true)
+				vim.api.nvim_feedkeys(keys, "n", false)
+			end)
+		end,
+	})
 end
 
 return M
